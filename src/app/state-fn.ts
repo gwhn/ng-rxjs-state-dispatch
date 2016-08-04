c3803ffbb4ee28e579478b28f34ba62e31843322
gwhn <gwhn@weblitz.co.uk>
2016-08-04 17:51:08 +0100

    Created todo-item component

--- src/app/state-fn.ts
+++ src/app/state-fn.ts
@@ -1,11 +1,11 @@
 import {AppState} from "./app-state";
 import {Action, TodoAction, FilterAction} from "./action";
-import {Observable, BehaviorSubject} from "rxjs";
 import {Todo} from "./todo";
 import {AddTodo} from "./add-todo";
 import {ToggleTodo} from "./toggle-todo";
 import {SetVisibilityFilter} from "./set-visibility-filter";
 import {OpaqueToken} from "@angular/core";
+import {Observable, BehaviorSubject} from "rxjs/Rx";
 
 export const stateFn = (initial: AppState, action$: Observable<Action>): Observable<AppState> => {
     const subject$ = new BehaviorSubject(initial);
@@ -60,12 +60,12 @@ const visibilityFilterReducer = (initial: string, action$: Observable<Action>): 
         }, initial);
 };
 
-export const initState = new OpaqueToken("initState");
+export const initialState = new OpaqueToken("initialState");
 export const dispatcher = new OpaqueToken("dispatcher");
 export const state = new OpaqueToken("state");
 export const stateAndDispatcher = [
     {
-        provide: initState,
+        provide: initialState,
         useValue: {todos: [], visibilityFilter: 'SHOW_ALL'}
     },
     {
@@ -75,6 +75,6 @@ export const stateAndDispatcher = [
     {
         provide: state,
         useFactory: stateFn,
-        deps: [initState, dispatcher]
+        deps: [initialState, dispatcher]
     }
 ];
