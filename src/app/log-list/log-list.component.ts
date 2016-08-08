5b8a4c815145794f5ad54196b31a96bbc4cc9914
gwhn <gwhn@weblitz.co.uk>
2016-08-08 15:17:54 +0100

    Made log-list component scroll to bottom to show latest message

--- src/app/log-list/log-list.component.ts
+++ src/app/log-list/log-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, AfterViewChecked, AfterViewInit} from '@angular/core';
 import {TodoService} from "../todo.service";
 
 @Component({
@@ -7,10 +7,24 @@ import {TodoService} from "../todo.service";
     templateUrl: 'log-list.component.html',
     styleUrls: ['log-list.component.css']
 })
-export class LogListComponent {
+export class LogListComponent implements AfterViewInit, AfterViewChecked {
     log$;
 
-    constructor(private service: TodoService) {
+    constructor(private service: TodoService,
+                private el: ElementRef) {
         this.log$ = service.log$;
     }
+
+    private ol: HTMLElement;
+
+    ngAfterViewInit() {
+        this.ol = this.el.nativeElement.querySelector('ol');
+    }
+
+    ngAfterViewChecked() {
+        if (this.ol) {
+            this.ol.scrollTop = this.ol.scrollHeight;
+        }
+    }
 }
+
